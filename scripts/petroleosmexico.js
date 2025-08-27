78f73020f9000706737bce1aa8e81a81aeb31f57
LikeThisSheep <enriquenunez9996@hotmail.com>
2025-08-27 10:15:21 -0400

    Update: Added timestamp in the page
    I added timestamp for only testing propuses

--- scripts/petroleosmexico.js
+++ scripts/petroleosmexico.js
@@ -1,3 +1,6 @@
 console.log("Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 let mensaje = "EL TITAN DE LA PROGRAMACIÓN"
 document.writeln("<h3>"+mensaje+ "</h3>");
+const timestamp = Date.now();
+mensajee = "Hoy es timestamp: "+timestamp;
+document.writeln("<p>" + mensajee + "</p>")
